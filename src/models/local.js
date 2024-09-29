cc7f8f866ff38538b1af2666ddee6eb71a3d4cf8
[calexd] <[caledarvs@gmail.com]>
2024-09-29 05:32:24 -0300

    switch to Google API for more accurate address retrieval, add new columns to location table, update functions in local controller

--- src/models/local.js
+++ src/models/local.js
@@ -1,14 +1,26 @@
 const { DataTypes } = require("sequelize");
 const connection = require("../database/connection");
 
-const Local = connection.define("locations", {
-  nome: { type: DataTypes.STRING, allowNull: false },
-  descricao: DataTypes.STRING,
-  cep: { type: DataTypes.STRING, allowNull: false },
-  user_id: { type: DataTypes.INTEGER, allowNull: false },
-  localidade: DataTypes.STRING,
-  coordenadas: DataTypes.TEXT,
-  googleMapsLink: DataTypes.STRING,
-});
+const Local = connection.define(
+  "Local",
+  {
+    nome: { type: DataTypes.STRING, allowNull: false },
+    descricao: DataTypes.STRING,
+    cep: { type: DataTypes.STRING, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    cep: DataTypes.STRING,
+    bairro: DataTypes.STRING,
+    logradouro: DataTypes.STRING,
+    localidade: DataTypes.STRING,
+    estado: DataTypes.STRING,
+    numero: DataTypes.INTEGER,
+    coordenadas: DataTypes.TEXT,
+    googleMapsLink: DataTypes.STRING,
+  },
+  {
+    tableName: "Locations",
+  }
+);
+
 
 module.exports = Local;
